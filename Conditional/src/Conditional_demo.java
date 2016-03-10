4901f1e3cee2646e2a92a1ab1281f3d6a7e0e89f
Karan <Karan@Karan-PC>
2016-03-10 11:19:38 +0545

    conditional

---
+++ Conditional/src/Conditional_demo.java
@@ -0,0 +1,18 @@
+public class Conditional_demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int a,b;
+		a=30;
+		b=(a==10)?20:10;//(val=(val==value)? if_true:if_false)
+		System.out.println("b="+b);
+		b=(a==30)?20:10;
+		System.out.println("b="+b);
+	}	
+
+	
+	}
+
