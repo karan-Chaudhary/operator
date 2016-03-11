942c4a7662084d852775a38a4dbc8ee77c31598f
Karan Jaiswal <karanjaiswal632@gmail.com>
2016-03-11 04:36:38 +0100

    Added files via upload

---
+++ Asignment.java
@@ -0,0 +1,21 @@
+public class Asignment{
+	public static void main(String args[]){
+
+		int a=10;
+		int b=20;
+		int c=10;
+
+		c+=a;
+		System.out.println("c+=a="+c);
+		c-=a;
+		System.out.println("c-=a="+c);
+		c*=a;
+		System.out.println("c*=a="+c);
+		c=30;
+		c/=a;
+		System.out.println("c/=a="+c);
+		c=30;
+		c%=a;
+		System.out.println("c%=a="+c);
+	}
+}
