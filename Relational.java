21dff81f744c20e0808ac60339cda96a065f7f26
Karan Jaiswal <karanjaiswal632@gmail.com>
2016-03-11 04:53:18 +0100

    Relation Operators

---
+++ Relational.java
@@ -0,0 +1,18 @@
+public class Relational{
+
+	public static void main(String args[]){
+		int a=10;int b=20;
+		if(a>b){
+		System.out.println("a is greater than b");
+			}
+	else
+		System.out.println("b is greater than a");
+
+		System.out.println("a==b="+(a==b));
+		System.out.println("a!=b="+(a!=b));
+		System.out.println("a>b="+(a>b));
+		System.out.println("a<b="+(a<b));
+		System.out.println("b>=a="+(b>=a));
+		System.out.println("b<=a="+(b<=a));
+	}
+}
